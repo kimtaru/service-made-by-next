77eab288fd0779710ca0dcd7cf26196d3c09ee19
kimtaru <kimtaru2@gmail.com>
2022-04-06 20:06:14 +0900

    -

--- express/app.js
+++ express/app.js
@@ -11,6 +11,8 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 dotenv.config();
 
@@ -23,10 +25,18 @@ db.sequelize
   .catch(console.error);
 
 passportConfig();
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(
   cors({
-    origin: "http://localhost:3060",
+    origin: ["http://localhost:3060", "tarusns.com"],
     credentials: true,
   })
 );
